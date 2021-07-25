9fef967c5ccc053ca3e52f34d012950b0a8c7d67
Purdbull <72924017+Purdbull@users.noreply.github.com>
2021-07-25 12:22:35 +0200

    added sites

--- ikindo/pages/views.py
+++ ikindo/pages/views.py
@@ -10,8 +10,14 @@ def index(request):
 def biography(request):
     return render(request, 'ikindo/biography.html', {'base': BASE_FILE})
 
-def hoerbeispiele(request):
-    return render(request, 'ikindo/hoerbeispiele.html', {'base': BASE_FILE})
+def samples(request):
+    return render(request, 'ikindo/samples.html', {'base': BASE_FILE})
 
 def repertoire(request):
     return render(request, 'ikindo/repertoire.html', {'base': BASE_FILE})
+
+def compositions(request):
+    return render(request, 'ikindo/compositions.html', {'base': BASE_FILE})
+
+def news(request):
+    return render(request, 'ikindo/news.html', {'base': BASE_FILE})
