f00d75f372b4840d7bd1d58e8c9d9e7aebfdfc0b
schmida736 <schmidtd.mail@gmail.com>
2021-08-28 14:52:51 +0200

    preparing for merge

--- ikindo/ikindo/settings.py
+++ ikindo/ikindo/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+6dqsqv5-i=p%yolmt+x-p&#)k#g5em8iqe%l1ot=3cc^+6an2'
+SECRET_KEY = 'yja-oc7=df!n!y&&_&fa3r=vytlpelqx8%k)^bn$(xuu1k-#vt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mancusi.at']
 
 
 # Application definition
